5c9f87a8824e2a2aa9548738ddf1ec91d30afab4
Althaf Nazeer <119403840+Althaf-Nazeer@users.noreply.github.com>
2023-10-24 22:50:50 +0530

    Create DFSingraph.py

---
+++ DFSingraph.py
@@ -0,0 +1,6 @@
+def dfs(graph, node, visited):
+    if node not in visited:
+        print(node, end=' ')
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs(graph, neighbor, visited)
