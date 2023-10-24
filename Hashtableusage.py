9bad6c04ffd6e52326c1f1edaef6912ff990b966
Althaf Nazeer <119403840+Althaf-Nazeer@users.noreply.github.com>
2023-10-24 22:50:16 +0530

    Create Hashtableusage.py

---
+++ Hashtableusage.py
@@ -0,0 +1,12 @@
+my_dict = {}
+my_dict['name'] = 'John'
+my_dict['age'] = 30
+
+print("Name:", my_dict['name'])
+print("Age:", my_dict['age'])
+my_dict = {}
+my_dict['name'] = 'John'
+my_dict['age'] = 30
+
+print("Name:", my_dict['name'])
+print("Age:", my_dict['age'])
