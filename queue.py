ea68272e42f66cdaccabc2b5942d78d0b5789423
Althaf Nazeer <119403840+Althaf-Nazeer@users.noreply.github.com>
2023-10-24 22:48:44 +0530

    Create queue.py

---
+++ queue.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self.items = deque()
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.popleft()
+        return None
+
+    def is_empty(self):
+        return len(self.items) == 0
