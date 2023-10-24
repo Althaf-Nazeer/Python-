85da42d8b0a0c4a88c24b1299276f9407ff98f13
Althaf Nazeer <119403840+Althaf-Nazeer@users.noreply.github.com>
2023-10-24 22:52:09 +0530

    Create Dijkstrasalgorithm.py

---
+++ Dijkstrasalgorithm.py
@@ -0,0 +1,19 @@
+import heapq
+
+def dijkstra(graph, start):
+    distances = {node: float('infinity') for node in graph}
+    distances[start] = 0
+    priority_queue = [(0, start)]
+
+    while priority_queue:
+        current_distance, current_node = heapq.heappop(priority_queue)
+
+        if current_distance > distances[current_node]:
+            continue
+
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+    return distances
