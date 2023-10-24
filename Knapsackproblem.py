d027c4a5225a9f1af42a9407a2d19d2c2d2cf0fa
Althaf Nazeer <119403840+Althaf-Nazeer@users.noreply.github.com>
2023-10-24 22:52:34 +0530

    Create Knapsackproblem.py

---
+++ Knapsackproblem.py
@@ -0,0 +1,7 @@
+def knapsack(values, weights, capacity):
+    n = len(values)
+    dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+
+    for i in range(n + 1):
+        for w in range(capacity + 1):
+            if i == 0 or w
