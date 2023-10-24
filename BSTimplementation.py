b317d12204540845e9b1ee8edd1b25cd42ef6c30
Althaf Nazeer <119403840+Althaf-Nazeer@users.noreply.github.com>
2023-10-24 22:49:12 +0530

    Create BSTimplementation.py

---
+++ BSTimplementation.py
@@ -0,0 +1,21 @@
+class TreeNode:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def insert(root, key):
+    if root is None:
+        return TreeNode(key)
+    if key < root.val:
+        root.left = insert(root.left, key)
+    else:
+        root.right = insert(root.right, key)
+    return root
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if root.val < key:
+        return search(root.right, key)
+    return search(root.left, key)
