9c7b9cbcf6434472591b20f1dba7e52770029872
Althaf Nazeer <119403840+Althaf-Nazeer@users.noreply.github.com>
2023-10-26 22:39:32 +0530

    Create trianglepattern.py

---
+++ trianglepattern.py
@@ -0,0 +1,11 @@
+def myfunc(n):
+    k = n - 1
+    for i in range(0, n):
+        for j in range(0, k):
+            print(end=" ")
+        k = k - 1
+        for j in range(0, i+1):
+            print("* ", end="")
+        print("\r")
+n = 5
+myfunc(n)
